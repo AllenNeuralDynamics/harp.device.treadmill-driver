abc360ffdd908c4416294c2cf1c294733a30f3d7
Joshua Vasquez <joshua.vasquez@alleninstitute.org>
2023-10-20 16:50:57 -0700

    update schematic

--- firmware/inc/config.h
+++ firmware/inc/config.h
@@ -1,11 +1,15 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#define TORQUE_LOAD_ADC_PIN 26
-#define ENCODER_BASE_PIN 0
+#define TORQUE_LOAD_ADC_PIN (26)
+#define ENCODER_BASE_PIN (0)
+
+#define HARP_SYNC_RX_PIN (5)
 
 
-#define ADC_INDEX 0
-#define TORQUE_PWM_PIN 2
+#define ADC_INDEX (0)
+#define TORQUE_PWM_PIN (2)
+
+#define TREADMILL_HARP_DEVICE_ID (0x0579)
 
 #endif // CONFIG_H
--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -3,66 +3,112 @@
 #include <cstdio> // for printf
 #include <pio_encoder.h>
 #include <analog_load_cell.h>
-#include <config.h>
 #include <continuous_adc_polling.h>
+#include <config.h>
+#include <harp_message.h>
 #include <harp_core.h>
+#include <harp_c_app.h>
 #include <harp_synchronizer.h>
 
 // Create device name array.
-const uint16_t who_am_i = 1234;
-const uint8_t hw_version_major = 1;
+const uint16_t who_am_i = TREADMILL_HARP_DEVICE_ID;
+const uint8_t hw_version_major = 0;
 const uint8_t hw_version_minor = 0;
-const uint8_t assembly_version = 2;
-const uint8_t harp_version_major = 2;
+const uint8_t assembly_version = 0;
+const uint8_t harp_version_major = 0;
 const uint8_t harp_version_minor = 0;
-const uint8_t fw_version_major = 3;
+const uint8_t fw_version_major = 0;
 const uint8_t fw_version_minor = 0;
-const uint16_t serial_number = 0xCAFE;
+const uint16_t serial_number = 0;
 
-// Create Core and synchronizer.
-HarpCore& core = HarpCore::init(who_am_i, hw_version_major, hw_version_minor,
-                                assembly_version,
-                                harp_version_major, harp_version_minor,
-                                fw_version_major, fw_version_minor,
-                                serial_number, "Harp Treadmill");
+// Setup for Harp App
+const size_t reg_count = 1;
+
+#pragma pack(push, 1)
+struct app_regs_t
+{
+    volatile uint32_t encoder_raw;
+    volatile uint16_t torque_midpoint;
+    volatile uint16_t torque_raw;
+    volatile uint16_t torque_setpoint;
+    volatile uint8_t brake_enabled;
+    // More app "registers" here.
+} app_regs;
+
+// Define "specs" per-register
+RegSpecs app_reg_specs[reg_count]
+{
+    {(uint8_t)&app_regs.encoder_raw, sizeof(app_regs.encoder_raw), U32},
+    {(uint8_t)&app_regs.torque_midpoint, sizeof(app_regs.torque_midpoint), U16},
+    {(uint8_t)&app_regs.torque_raw, sizeof(app_regs.torque_raw), U16},
+    {(uint8_t)&app_regs.torque_setpoint, sizeof(app_regs.torque_setpoint), U16},
+    {(uint8_t)&app_regs.brake_enabled, sizeof(app_regs.brake_enabled), U8}
+    // More specs here if we add additional registers.
+}
+
+RegFnPair reg_handler_fns[reg_count]
+{
+
+    {&HarpCore::read_reg_generic, &HarpCore::write_to_read_only_reg_error},
+    {&HarpCore::read_reg_generic, &HarpCore::write_to_read_only_reg_error},
+    {&HarpCore::read_reg_generic, &HarpCore::write_to_read_only_reg_error},
+    {&HarpCore::read_reg_generic, &HarpCore::write_reg_generic},
+    {&HarpCore::read_reg_generic, &HarpCore::write_reg_generic}
+    // More handler function pairs here if we add additional registers.
+}
+
+void update_app_state
+{
+    // Called periodically inside of app.run()
+    // Nothing to do!
+}
+
+void reset_app
+{
+    // Called when we write to the core reset "register."
+    app_regs.torque_setpoint = app_regs.torque_midpoint;
+}
+
+// Create Core.
+HarpCApp& app = HarpCApp::init(who_am_i, hw_version_major, hw_version_minor,
+                               assembly_version,
+                               harp_version_major, harp_version_minor,
+                               fw_version_major, fw_version_minor,
+                               serial_number, "Harp Treadmill",
+                               &app_regs, app_reg_specs,
+                               reg_handler_fns, reg_count, update_app_state,
+                               reset_app);
+
+// Create ADs70x9 instance for the ADS7049.
+PIO_ADS70x9 ads7049(pio0, 0, 12, ADS7049_CS_PIN, ADS7049_SCK_PIN,
+                    ADS7049_POCI_PIN);
 
 // Core0 main.
 int main()
 {
 #ifdef DEBUG
     stdio_uart_init_full(uart1, 921600, 4, -1); // use uart1 tx only.
-    printf("Hello, from a Pi Pico!\r\n");
+    printf("Hello, from an RP2040!\r\n");
 #endif
+    // Init Synchronizer.
+    HarpSynchronizer& sync = HarpSynchronizer::init(uart0, HARP_SYNC_RX_PIN);
+    // Init PIO encoder on pins 0 and 1.
+    // TODO: Setup DMA to write PIO to memory.
+    PIOEncoder encoder(0, 0, ENCODER_BASE_PIN);
+    //encoder.setup_dma_stream_to_memory(&encoder_raw, 1);
+    //encoder.start();
 
-    while(true)
-    {
-        core.run();
-    }
-/*
     // Setup DMA channels to continuously stream ADC data to memory.
-    setup_continuous_adc_polling();
+    //setup_continuous_adc_polling();
+    // Init PIO-based ADC with continuous streaming to memory via DMA..
+    ads7049.setup_dma_stream_to_memory(&torque_raw, 1);
+    ads7049.start();
 
     // Init Torque Load Controller.
-    CurrentDriver(&adc_vals[ADC_INDEX], TORQUE_PWM_PIN);
-
-    uint32_t count, torque;
-    stdio_usb_init(); // TODO: remove this when we add back harp core.
-    while (!stdio_usb_connected()){sleep_ms(1);}
-
-    // Init PIO encoder on pins 0 and 1.
-    PIOEncoder encoder(0, 0, ENCODER_BASE_PIN);
-    AnalogLoadCell torque_load(TORQUE_LOAD_ADC_PIN);
+    //CurrentDriver(&adc_vals[ADC_INDEX], TORQUE_PWM_PIN);
 
     while(true)
     {
-        //core.run();
-        // Update State. Handle events.
-        // Read encoder inputs.
-        //count = encoder.get_count();
-        torque = torque_load.read_raw();
-        printf("encoder: %09lu | torque: %09lu\r", count, torque);
-        sleep_ms(17); // ~60[Hz] refresh rate.
+        app.run();
     }
-    return 0;
-*/
 }
