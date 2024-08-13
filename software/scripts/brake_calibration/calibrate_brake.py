bdef846099e551f41aa6491f62c16dcffc5d6e57
Sonya Vasquez <sonya.vasquez@alleninstitute.org>
2024-08-13 13:24:51 -0700

    draft brake calibration scripts

---
+++ software/scripts/brake_calibration/calibrate_brake.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Calibration Script for extacting raw data to linearize magnetic brake."""
+
+def set_signed_speed_percent(percent: float):
+    # Map values to signed range 0 - 4095 where 2048 is midscale.
+    pass
+
+
+
+
+if __name__ == "__main__":
+    input("Press enter to begin calibration.")
+    # TODO: Periodically updated Matplotlib Python graph here.
+    #   (Maybe use GR if latency is an issue?)
+    pass
