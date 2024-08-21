5c4ff24999c55b5d41d154e3d0745b8a3eeb1fb9
Sonya Vasquez <sonya.vasquez@alleninstitute.org>
2024-08-20 17:24:31 -0700

    draft working calibration script

--- software/scripts/brake_calibration/calibrate_brake.py
+++ software/scripts/brake_calibration/calibrate_brake.py
@@ -20,6 +20,7 @@ import numpy as np
 
 ALL_SENSORS_REG = 35
 BRAKE_CURRENT_SETPOINT_REG = 37
+TORQUE_LIMITING_TRIGGERED_REG = 41
 
 device = None
 
@@ -65,8 +66,11 @@ def set_signed_motor_speed(percent: float):
 
 def set_brake_current_raw(raw_current):
     """Set the brake current in raw uint16 format."""
-    reply = device.send(HarpMessage.WriteU16(BRAKE_CURRENT_SETPOINT_REG,
-                                             raw_current).frame)
+    device.send(HarpMessage.WriteU16(BRAKE_CURRENT_SETPOINT_REG,
+                                     raw_current).frame)
+
+def clear_treadmill_overtorque_errors():
+    device.send(HarpMessage.WriteU8(TORQUE_LIMITING_TRIGGERED_REG, 0).frame)
 
 def get_measurements() -> list:
     """Read the [position, torque, current] measurements from the board."""
@@ -83,7 +87,7 @@ if __name__ == "__main__":
                         help="Harp Treadmill Driver com port")
     parser.add_argument("--min_current", type=float, default=0,
                         help="minimum current value in [%]")
-    parser.add_argument("--max_current", type=float, default=50,
+    parser.add_argument("--max_current", type=float, default=100,
                         help="maximum current value in [%]")
     parser.add_argument("--sample_average_count", type=int, default=10,
                         help="number of samples to average together "
@@ -92,32 +96,47 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     # Connect to Harp Treadmill Device.
-    #try:
-    #    device = Device(args.port, "/dev/null")
-    #except SerialException as e:
-    #    print("Cannot connect to Harp Treadmill device! Is it plugged in and "
-    #          "powered on? Is the com port correct?")
+    try:
+        device = Device(args.port, "/dev/null")
+    except SerialException as e:
+        print("Cannot connect to Harp Treadmill device! Is it plugged in and "
+              "powered on? Is the com port correct?")
+        sys.exit(1) # Return with error.
+
 
     # Configure motor controller
-    settings = get_motor_controller_settings()
-    set_motor_to_open_loop_mode(settings)
-    apply_motor_controller_settings(settings)
+    #settings = get_motor_controller_settings()
+    #set_motor_to_open_loop_mode(settings)
+    #apply_motor_controller_settings(settings)
+    sleep(0.1)
 
-    set_signed_motor_speed(50.0) # %
+    clear_treadmill_overtorque_errors()
+    print("Setting speed to midscale.")
+    set_signed_motor_speed(75.0)
+    sleep(1.0)
 
-    #max_uint16 = np.iinfo(np.uint16).max
-    ## Range should be in steps of 4 since the value is 12-bit upscaled to 16-bit
-    #input_current = np.arange(0, round(max_uint16/args.max_current), 4)
-    #output_torque = np.zeros(len(input_current))
+    try:
+        max_uint16 = np.iinfo(np.uint16).max
+        # Range should be in steps of 4 since the value is 12-bit upscaled to 16-bit
+        input_current = np.arange(0, round(max_uint16*args.max_current/100.), 4)
+        output_torque = np.zeros(len(input_current))
+        print(input_current)
 
-    #for i, current in enumerate(tqdm(input_current)):
-    #    set_brake_current_raw(current)
-    #    raw_pts = []
-    #    for _ in range(args.sample_average_count):
-    #        sleep(0.001)
-    #        raw_pts.append(get_measurements()[1])
-    #    output_torque[i] = float(sum(raw_pts))/args.sample_average_count
+        for i, current in enumerate(tqdm(input_current)):
+            set_brake_current_raw(int(current))
+            raw_pts = []
+            for _ in range(args.sample_average_count):
+                sleep(0.001)
+                raw_pts.append(get_measurements()[1])
+            output_torque[i] = float(sum(raw_pts))/args.sample_average_count
+        print()
+    except KeyboardInterrupt:
+        pass # abort early and print what we have.
+    finally:
+        print("Zeroing motor speed.")
+        set_signed_motor_speed(0) # %
+        set_brake_current_raw(0)
 
-    ## Plot Output current vs input current!
-    #plt.plot(input_current, output_torque, 'go--', linewidth=2, markersize=12)
-    #plt.show()
+    # Plot Output current vs input current!
+    plt.plot(input_current, output_torque, 'go--', linewidth=2, markersize=12)
+    plt.show()
