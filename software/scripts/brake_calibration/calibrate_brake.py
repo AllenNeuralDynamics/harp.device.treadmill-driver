f6622f656141ab50f6483f50d70a549c541990b1
Sonya Vasquez <sonya.vasquez@alleninstitute.org>
2024-08-26 13:26:38 -0700

    add duration calculation info msg

--- software/scripts/brake_calibration/calibrate_brake.py
+++ software/scripts/brake_calibration/calibrate_brake.py
@@ -3,6 +3,7 @@
 # Uses: Jrk G2 18v27 Pololu motor controller to apply torque to the brake.
 # https://www.pololu.com/docs/0J73
 
+from datetime import datetime
 from pyharp.device import Device
 from pyharp.messages import HarpMessage
 from pathlib import Path
@@ -12,7 +13,7 @@ from serial import SerialException
 from struct import unpack
 from tqdm import tqdm
 import matplotlib.pyplot as plt
-import argparse
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 import subprocess
 import os
 import sys
@@ -89,16 +90,22 @@ def get_measurements() -> list:
 
 if __name__ == "__main__":
     # Collect input params.
-    parser = argparse.ArgumentParser()
+    # Show defaults.
+    parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
     parser.add_argument("--port", type=str, default="/dev/ttyACM0",
                         help="Harp Treadmill Driver com port")
     parser.add_argument("--min_current", type=float, default=0,
-                        help="minimum current value in [%%]")
+                        help="minimum current value in percent of full scale "
+                             "range.")
     parser.add_argument("--max_current", type=float, default=40,
-                        help="maximum current value in [%%]")
-    parser.add_argument("--sample_average_count", type=int, default=10,
+                        help="maximum current value in percent of full scale "
+                             "range.")
+    parser.add_argument("--sample_average_count", type=int, default=30,
                         help="number of samples to average together "
-                             "*per* data point (default: 10)")
+                             "*per* data point.")
+    parser.add_argument("--time_delta", type=float, default=0.01,
+                        help="time (in seconds) between collecting samples to "
+                             "average.")
     parser.add_argument("--reverse", type=bool, default=False)
     parser.add_argument("--output_file", type=str, default=None)
     args = parser.parse_args()
@@ -111,7 +118,6 @@ if __name__ == "__main__":
               "powered on? Is the com port correct?")
         sys.exit(1) # Return with error.
 
-
     # Configure motor controller
     print("Reading motor settings.")
     settings = get_motor_controller_settings()
@@ -122,21 +128,30 @@ if __name__ == "__main__":
     apply_motor_controller_settings(settings)
     sleep(0.1)
     clear_treadmill_overtorque_errors()
-    print("Setting speed to midscale.")
-    set_signed_motor_speed(100.)
+    print("Setting testing motor to max open loop speed.")
+    reverse = -1.0 if args.reverse else 1.0
+    set_signed_motor_speed(100. * reverse)
     sleep(1.0)
 
+    # Make X/Y ranges.
     max_uint16 = np.iinfo(np.uint16).max
     # Range should be in steps of 4 since the value is 12-bit upscaled to 16-bit
     input_current = np.arange(0, round(max_uint16*args.max_current/100.), 4)
     output_torque = np.zeros(len(input_current))
 
+    print("Starting calibration at "
+          f"{datetime.now().strftime('%Y-%m-%d, %I:%M %p')}")
+    eta_mins = round(args.sample_average_count * args.time_delta
+                     * len(input_current) / 60)
+    print(f"Calibration will take at least {eta_mins} minutes.")
+
+    # Collect data.
     try:
         for i, current in enumerate(tqdm(input_current)):
             set_brake_current_raw(int(current))
             raw_pts = []
             for _ in range(args.sample_average_count):
-                sleep(0.05)
+                sleep(args.time_delta)
                 raw_pts.append(get_measurements()[1])
             output_torque[i] = float(sum(raw_pts))/args.sample_average_count
         print()
@@ -155,7 +170,7 @@ if __name__ == "__main__":
     # Plot Output current vs input current!
     plt.title("Brake Torque vs Input Current")
     plt.xlabel(f"input torque [0:{args.max_current}%] Full-Scale Range")
-    plt.ylabel("brake torque [0:4095] FSR w/2048 midscale")
+    plt.ylabel("brake torque [0:4095]\r\nFull-Scale Range w/2048 midscale")
     plt.plot(input_current, output_torque)
     plt.show()
 
